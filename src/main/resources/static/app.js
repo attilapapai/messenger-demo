4430b895d884251476303aab130f4590222a0a71
Attila Pápai <attilapapai@users.noreply.github.com>
2018-01-30 09:57:26 +0100

    removed unused code in app.js

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,22 +1,9 @@
 var stompClient = null;
 
-function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#greetings").html("");
-}
-
 function connect() {
     var socket = new SockJS('/messenger-demo');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
-        setConnected(true);
         console.log('Connected: ' + frame);
         stompClient.subscribe('/messenger', function (greeting) {
             showGreeting(JSON.parse(greeting.body).content);
